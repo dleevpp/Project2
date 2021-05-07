180357d21533c26d707812053a8699f9bf06d134
gggg8657 <55835288+gggg8657@users.noreply.github.com>
2021-05-08 00:08:46 +0900

    Update main.c
    time1 == null print error

--- main.c
+++ main.c
@@ -50,13 +50,21 @@ void filestat2(){
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
     time1 = localtime(&stat1.st_mtime);
-	if(time1==null)_exit(0);
+	if(time1==null)
+	{
+		printf("time1 is null");
+		_exit(0);
+	}	
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
 	time2 = localtime(&stat2.st_mtime);
-    if(time2==null)_exit(0);
+    if(time2==null)
+    {
+	    printf("time2 is null");
+	    _exit(0);
+    }
 }
 
 //두 개의 파일 크기를 비교하는 함수 작성
